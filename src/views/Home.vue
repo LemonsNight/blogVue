f218a27812c18d6b52f69185f8c335d738dfde71
inoryEnglish <512469231@qq.com>
2021-09-15 11:04:40 +0800

    修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    123
+    123123
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
